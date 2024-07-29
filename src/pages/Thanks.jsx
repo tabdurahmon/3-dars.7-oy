4b19d48ae2b6c3fa005038bf3880d462b84121c4
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-07-29 15:59:25 +0500

    add

--- src/pages/Thanks.jsx
+++ src/pages/Thanks.jsx
@@ -2,10 +2,12 @@ import thanks from "../../public/images/icon-thank-you.svg";
 
 function Thanks() {
   return (
-    <div className="pt-14  w-full">
-      <img className="w-[80px] mb-[80px]" src={thanks} alt="" />
-      <h1 className="text-4xl text-[#022959]   font-bold mb-3">Thank You</h1>
-      <p className="text-xl text-[#022959]   opacity-50 mb-10">
+    <div className="   ml-[100px] mt-[121px]">
+      <img className="w-[80px]  mb-[80px] ml-[185px]" src={thanks} alt="" />
+      <h1 className="text-4xl  text-[#022959]   font-bold mb-3 w-[450px] text-center">
+        Thank You
+      </h1>
+      <p className="text-xl text-[#022959] mt-[14px]   opacity-50 mb-10 w-[450px] text-center">
         Thanks for confirming your subscription! We hope you have fun using our
         platform. If you ever need support, please feel free to email us at
         support@loremgaming.com.
