3b38009bb7b44093717f1ff3c26f62e36e8f4c29
tabdurahmon <dilsozxonxatamova@gmail.com>
2024-07-29 16:03:50 +0500

    add

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,10 +1,8 @@
 function FormInput({ label, type, name, placeholder }) {
   return (
-    <label className="form-control text-[#022959] bg-white   w-full mb-6">
+    <label className="form-control  bg-white   w-full mb-6">
       <div className="label">
-        <span className="label-text text-[#022959]   font-medium mb-2">
-          {label}
-        </span>
+        <span className="label-text    font-medium mb-2">{label}</span>
       </div>
       <input
         type={type}
